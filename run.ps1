eca7c5063aefe1b4e58385b29077a7fe246be7b5
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-01-16 22:05:56 +0300

    Support killing all subprocesses using CTRL+C

--- run.ps1
+++ run.ps1
@@ -1,8 +1,31 @@
+# Change the default behavior of CTRL-C so that the script can intercept and use it versus just terminating the script.
+[Console]::TreatControlCAsInput = $True
+# Sleep for 1 second and then flush the key buffer so any previously pressed keys are discarded and the loop can monitor for the use of
+#   CTRL-C. The sleep command ensures the buffer flushes correctly.
+Start-Sleep -Seconds 1
+$Host.UI.RawUI.FlushInputBuffer()
+
 $FlaskProcess = Start-Job -Name Backend  -ScriptBlock { cd "${using:PWD}\backend"; $env:FLASK_RUN_PORT=7507; pipenv run flask run }
 $NodeProcess  = Start-Job -Name Frontend -ScriptBlock { cd "${using:PWD}\frontend"; $env:PORT=3000; npm run dev }
 $NginxProcess = Start-Job -Name Nginx    -ScriptBlock { cd "${using:PWD}"; $env:PORT=80; nginx -c conf/nginx.conf }
 Clear-Host
 Get-Job
-# TODO: Catch CTRL+C to terminate subprocesses
-# TODO: Create some interface to check the processes' outputs/statuses
-#     with error reporting in the infinite loop
+
+while ($true)
+{
+	# TODO: Create some interface to check the processes' outputs/statuses
+	#     with error reporting in the infinite loop
+    if ($Host.UI.RawUI.KeyAvailable)
+    {
+		$key = $Host.UI.RawUI.ReadKey("AllowCtrlC, IncludeKeyDown, IncludeKeyUp")
+		if($key.VirtualKeyCode -eq 67 -and $key.ControlKeyState -eq "LeftCtrlPressed")
+		{
+			Write-Host "Exiting gracefully..."
+			nginx -s quit
+			Remove-Job -Force $NginxProcess
+			Remove-Job -Force $FlaskProcess
+			Remove-Job -Force $NodeProcess
+			break;
+		}
+    }
+}
--- run.sh
+++ run.sh
@@ -1,7 +1,22 @@
 cd backend
 FLASK_RUN_PORT=7507 flask run&
+flask_pid=$!
 cd ../frontend
 PORT=3000 npm run dev&
+node_pid=$!
 cd ..
 PORT=80 nginx -c conf/nginx.conf&
-# TODO: find a way to stop all at once
+nginx_pid=$!
+
+kill_processes() {
+    printf "\rExiting\n"
+	pkill -P $$
+	exit
+}
+
+trap 'kill_processes' SIGINT
+
+input="$@"
+while true; do
+    read input
+done
